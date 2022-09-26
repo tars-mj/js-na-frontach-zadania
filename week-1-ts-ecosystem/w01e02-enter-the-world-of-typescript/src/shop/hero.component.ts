1016d10abbed9f0952ccea3769e433a266a136e4
Maciej Judka <maciej.judka@gmail.com>
2022-09-26 12:10:35 +0200

    refactor: hero.component to ts

---
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/hero.component.ts
@@ -0,0 +1,18 @@
+import { div, p, section } from '../framework/dom-creators.js';
+
+type Hero = {
+  title: string;
+  subTitle: string;
+};
+
+export function hero({ title, subTitle }: Hero) {
+  const $heroSection = section('hero is-link');
+  const $heroBody = div('hero-body');
+  const $title = p('title');
+  $title.innerText = title;
+  const $subTitle = p('subtitle');
+  $subTitle.innerText = subTitle;
+  $heroBody.append($title, $subTitle);
+  $heroSection.append($heroBody);
+  return $heroSection;
+}
