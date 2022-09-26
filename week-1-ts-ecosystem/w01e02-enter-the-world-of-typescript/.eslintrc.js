b86abbb9ba1d88dca158816a2f7dcde0b693f0dc
Maciej Judka <maciej.judka@gmail.com>
2022-09-26 11:19:24 +0200

    ci: add typescript

--- week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/.eslintrc.js
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/.eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-  },
-  extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:jsx-a11y/recommended', 'eslint-config-prettier'],
-  rules: {},
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {},
 };
