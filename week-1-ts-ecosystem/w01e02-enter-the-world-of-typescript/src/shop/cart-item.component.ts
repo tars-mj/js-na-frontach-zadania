18c63b6a6a5067622f686589cd6ae3a44d800883
Maciej Judka <maciej.judka@gmail.com>
2022-09-26 12:18:02 +0200

    refactor: cart-item.component to ts

---
+++ week-1-ts-ecosystem/w01e02-enter-the-world-of-typescript/src/shop/cart-item.component.ts
@@ -0,0 +1,16 @@
+import { div } from '../framework/dom-creators.js';
+import { Item } from '../types/item.model.js';
+
+export function cartItem({ name, amount, unit, price }: Item) {
+  const $panelBlock = div('panel-block');
+  const $name = div();
+  $name.textContent = name;
+  const $amount = div('ml-auto');
+  $amount.textContent = amount.toString();
+  const $unit = div('tag');
+  $unit.textContent = unit;
+  const $price = div('ml-4');
+  $price.textContent = `${price.value} ${price.currency}`;
+  $panelBlock.append($name, $amount, $unit, $price);
+  return $panelBlock;
+}
